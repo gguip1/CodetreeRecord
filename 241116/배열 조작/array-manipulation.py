5bb4dcd5071e94fd346fea48736e6073ba877cde
gguip1 <51047947+gguip1@users.noreply.github.com>
2024-11-16 20:11:52 +0900

    Title: 배열 조작, Time: 242ms, Memory: 20MB, Status: Passed - CodeTree

--- 241116/배열 조작/array-manipulation.py
+++ 241116/배열 조작/array-manipulation.py
@@ -3,11 +3,15 @@ a_size = int(input())
 a_ = list(map(int, input().split()))
 
 for index, value in enumerate(op):
-    if value == 'R':
-        a_.reverse()
-    elif value == 'D':
-        a_.pop(0)
-        a_size = len(a_)
+    if a_size > 0:
+        if value == 'R':
+            a_.reverse()
+        elif value == 'D':
+            a_.pop(0)
+            a_size = len(a_)
+    else:
+        print('error')
+        break
 
 for i in a_:
     print(f'{i}', end=' ')
