37a048e36b3066c7beb601de818c3c071948c5bc
gguip1 <51047947+gguip1@users.noreply.github.com>
2024-11-17 03:25:30 +0900

    Title: 수열 맞추기, Time: 5019ms, Memory: 38MB, Status: Time Limit Exceed - CodeTree

--- 241117/수열 맞추기/backtracking-the-sequence.py
+++ 241117/수열 맞추기/backtracking-the-sequence.py
@@ -1,17 +1,19 @@
-from collections import deque
-
 n = int(input())
 a = list(map(int, input().split()))
 a.reverse()
 
-result_deque = deque()
+result_array = []
 
 for index, value in enumerate(a):
-    if value == 1:
-        result_deque.appendleft(index + 1)  # 맨 앞에 삽입
-    elif value == 2:
-        result_deque.insert(1, index + 1)  # 두 번째에 삽입
-    elif value == 3:
-        result_deque.append(index + 1)  # 맨 뒤에 삽입
+    if len(result_array) >= 1:
+        if value == 1:
+            result_array.insert(0, index + 1)
+        elif value == 2:
+            result_array.insert(1, index + 1)
+        elif value == 3:
+            result_array.append(index + 1)
+    else:
+        result_array.insert(0, index + 1)
 
-print(" ".join(map(str, result_deque)))
+for v in result_array:
+    print(v, end=" ")
