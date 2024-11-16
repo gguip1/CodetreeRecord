3372c375f6f404814d778fda5d520708565701f5
gguip1 <51047947+gguip1@users.noreply.github.com>
2024-11-17 03:18:54 +0900

    Title: 수열 맞추기, Time: 5087ms, Memory: 38MB, Status: Time Limit Exceed - CodeTree

--- 241117/수열 맞추기/backtracking-the-sequence.py
+++ 241117/수열 맞추기/backtracking-the-sequence.py
@@ -2,23 +2,18 @@ n = int(input())
 a = list(map(int, input().split()))
 a.reverse()
 
-array_ = []
-
-for i in range(n):
-    array_.append(i + 1)
-
 result_array = []
 
 for index, value in enumerate(a):
     if len(result_array) >= 1:
         if value == 1:
-            result_array.insert(0, array_[index])
+            result_array.insert(0, index + 1)
         elif value == 2:
-            result_array.insert(1, array_[index])
+            result_array.insert(1, index + 1)
         elif value == 3:
-            result_array.append(array_[index])
+            result_array.append(index + 1)
     else:
-        result_array.insert(0, array_[index])
+        result_array.insert(0, index + 1)
 
-for index, value in enumerate(result_array):
-    print(value, end=" ")
+for v in result_array:
+    print(v, end=" ")
