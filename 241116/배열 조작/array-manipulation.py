266290081e8504333512cb62dced139f623e47a3
gguip1 <51047947+gguip1@users.noreply.github.com>
2024-11-16 20:09:10 +0900

    Title: 배열 조작, Time: 78ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 241116/배열 조작/array-manipulation.py
@@ -0,0 +1,13 @@
+op = input()
+a_size = int(input())
+a_ = list(map(int, input().split()))
+
+for index, value in enumerate(op):
+    if value == 'R':
+        a_.reverse()
+    elif value == 'D':
+        a_.pop(0)
+        a_size = len(a_)
+
+for i in a_:
+    print(f'{i}', end=' ')
