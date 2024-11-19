839add33f5523e843811c547a4137b38f57bf03d
gguip1 <51047947+gguip1@users.noreply.github.com>
2024-11-19 23:42:01 +0900

    Title: 1까지 나누기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241119/1까지 나누기/divide-by-1.py
+++ 241119/1까지 나누기/divide-by-1.py
@@ -3,11 +3,10 @@ n = int(input())
 count = 0
 
 for i in range(n):
-    n /= (i + 1)
+    n //= (i + 1)
     if n > 1:
         count += 1
     else:
         count += 1
         break
-
 print(count)
