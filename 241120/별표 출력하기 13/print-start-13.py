38c014259dfaca6c46db29a05341e957679120b4
gguip1 <51047947+gguip1@users.noreply.github.com>
2024-11-20 20:43:42 +0900

    Title: 별표 출력하기 13, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241120/별표 출력하기 13/print-start-13.py
+++ 241120/별표 출력하기 13/print-start-13.py
@@ -6,7 +6,8 @@ t_1 = 1
 for i in range(n):
     if i % 2 == 0:
         print('* ' * t_0)
-        t_0 -= 1
+        if t_0 != 1:
+            t_0 -= 1
     else:
         print('* ' * t_1)
         t_1 += 1
