d5eff6955e5746dd46aec4fbcef178e2c96b08f6
gguip1 <51047947+gguip1@users.noreply.github.com>
2024-11-20 20:52:39 +0900

    Title: 별표 출력하기 13, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241120/별표 출력하기 13/print-start-13.py
+++ 241120/별표 출력하기 13/print-start-13.py
@@ -1,21 +1,20 @@
 n = int(input())
 
-t_0 = n
-t_1 = 1
+t_0 = n + 1
+t_1 = 0
 
 for i in range(n):
     if i % 2 == 0:
-        print('* ' * t_0)
-        if t_0 != 1:
-            t_0 -= 1
-    else:
-        print('* ' * t_1)
-        t_1 += 1
-
-for i in range(n):
-    if i % 2 == 0:
-        print('* ' * t_0)
         t_0 -= 1
+        print('* ' * t_0)
     else:
-        print('* ' * t_1)
         t_1 += 1
+        print('* ' * t_1)
+
+for i in range(n, 0, -1):
+    if i % 2 == 0:
+        t_0 -= 1
+        print('* ' * t_0)
+    else:
+        t_1 += 1
+        print('* ' * t_1)
