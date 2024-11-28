2c254599dba9d467bd44f3c2ac5798b22f94bf7d
gguip1 <51047947+gguip1@users.noreply.github.com>
2024-11-28 20:14:31 +0900

    Title: 정수의 최솟값, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

--- 241128/정수의 최솟값/minimum-value-of-an-integer.py
+++ 241128/정수의 최솟값/minimum-value-of-an-integer.py
@@ -7,10 +7,11 @@ def min_(list_):
         if min__ is None:
             min__ = __
             continue
+
         if min__ > __:
             min__ = __
         
-        return min__
+    return min__
 
 print(min_(_))
 
