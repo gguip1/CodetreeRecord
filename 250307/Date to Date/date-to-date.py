c7fd9e259229a6fbee296b1d385a85288cbb72d2
gguip1 <gguip7554@naver.com>
2025-03-07 21:59:06 +0900

    Title: Date to Date, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250307/Date to Date/date-to-date.py
@@ -0,0 +1,19 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+if m2 == m1:
+    print(d2 - d1)
+elif abs(m2 - m1) == 1:
+    print(months[m2 - 1] - d1 + d2)
+else:
+    days = 0
+    for i in range(m1, m2 + 1):
+        if i == m1:
+            days += months[i - 1] - d1
+        elif i == m2:
+            days += d2
+        else:
+            days += months[i]
+    print(days)
